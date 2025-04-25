7b920ca00e2bad2db27ed97d9579f6792e01e4f4
Derekan <55803665+Hunter-Derekan@users.noreply.github.com>
2025-04-24 22:07:32 -0400

    final touches, water shader added

--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
 import * as THREE from "three";
-
 import { GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 
 // Group 4
 // COSC3306 Final Project
+// Code by Ashlinn and Ethan. referenced links appear where they are implemented from
+// the shader turned out a bit underwhelming, but it does fulfill the basic function of moving water
+// the directional light unfortunately cant really be placed in front of the sun/moon
+// objects, so they will look dark no matter what, even though theyre "emitting" light
+// imported objects and textures are referenced in the report
+// made objects are denoted where found both here and in the reports
+// just be mindful that the initial value of the day and time need to be changed in order 
+// to take effect. for some reason, despite setting the initial value to day, it says "undefined"
+// but everything will work as intended once a value is set manually
 
 let scene, renderer, camera, gui;
+
+// used to determine shader calculations
+let TIME = 0.0;
+let windowSize = new THREE.Vector2(window.innerWidth, window.innerHeight);
 
 scene = new THREE.Scene();
 
@@ -29,6 +41,16 @@ const directionalLight = new THREE.DirectionalLight( 0xffffff, 5.0 );
 // instantiate ambient light
 const light = new THREE.AmbientLight( 0x404040, 10.0 ); 
 scene.add( light );
+// lamps at the house emit lights during night time
+const lampLightA = new THREE.PointLight( 0xfff8d6, 30.0, 750 );
+lampLightA.position.set( 36, -31, -8 );
+lampLightA.power = 350.0;
+const lampLightB = new THREE.PointLight( 0xfff8d6, 30.0, 750 );
+lampLightB.position.set( 20, -31, -24 );
+lampLightB.power = 350.0;
+
+scene.add( lampLightA );
+scene.add( lampLightB );
 
 // sun object
 const sun = new THREE.Mesh(new THREE.SphereGeometry(30, 64, 32), 
@@ -54,14 +76,18 @@ const params = {
 makeGUI();
 makeWorld();
 loadDuck(200, -99, 300);
+loadDuck(-150, -99, -200);
+loadDuck(-50, -99, -400);
+loadDuck(300, -99, 0);
 loadDock(205, -100, 0);
 loadBarrel(202, -86, 42);
 loadBarrel(202, -86, -42);
 loadHouse(0, -53, 0);
 loadTree(40, -80, 80);
-loadTree(100, -100, -100);
-loadLamp(36, -40.2, -8);
-loadLamp(20, -40.2, -24);
+loadTree(100, -110, -100);
+loadTree(-100, -90, -70);
+loadLamp(36, -26.5, -8);
+loadLamp(20, -26.5, -24);
 
 // creates GUI and its functions
 function makeGUI() {
@@ -73,20 +99,24 @@ function makeGUI() {
       console.log("day");
       light.intensity = 10.0;
       directionalLight.color.setHex(0xded899);
-      directionalLight.intensity = 7.0;
+      directionalLight.intensity = 6.0;
       sun.visible = true;
       moon.visible = false;
+      lampLightA.visible = false;
+      lampLightB.visible = false;
       skyBoxDay();
     }
     // during nighttime, ambient light and directional light settings are lower
     // to be darker, and loads in the night sky box/moon
     if (value === DayCycle.NIGHT) {
       console.log("night");
-      light.intensity = 4.0;
+      light.intensity = 3.0;
       directionalLight.color.setHex(0xcbf7f7);
-      directionalLight.intensity = 3.0;
+      directionalLight.intensity = 1.0;
       sun.visible = false;
       moon.visible = true;
+      lampLightA.visible = true;
+      lampLightB.visible = true;
       skyBoxNight();
     }
     scene.add( directionalLight );
@@ -94,6 +124,8 @@ function makeGUI() {
 
   gui.add(params, 'time', -1, 1).step(0.01).onChange(function (value) {
     // moves light and sun/moon according to time of day. the light and object move together
+    // 0 is basically noon/midnight, with values less or greater than 0 being changes
+    // in the time from there. the time determines the position of the sun/moon and lights
     if (value < 0) {
       directionalLight.position.x = value * 1000;
       directionalLight.position.y = 1100 + (1100 * value);
@@ -116,23 +148,23 @@ function makeGUI() {
 }
 
 // loads in duck objects (made by Ethan), placing them at the passed x/y/z coordinates
-function loadDuck(x, y , z) {
+function loadDuck(x, y, z) {
   const loader = new GLTFLoader().setPath( 'gltf/' );
   loader.load( 'rubberduck.glb', function ( gltf ) {
 
     const model = gltf.scene;
 
-    model.scale.set(5, 5, 5);
+    model.scale.set(15, 15, 15);
     model.position.x = x;
     model.position.y = y;
     model.position.z = z;
-    model.rotation.y = Math.random(Math.PI / 2);
+    model.rotation.y = Math.random(Math.PI / -2);
     scene.add( model );
 
   } );
 }
 // loads in the barrel objects, placed at the passed x/y/z coordinates
-function loadBarrel(x, y , z) {
+function loadBarrel(x, y, z) {
   const loader = new GLTFLoader().setPath( 'gltf/' );
   loader.load( 'Barrel.gltf', function ( gltf ) {
 
@@ -148,7 +180,7 @@ function loadBarrel(x, y , z) {
 }
 
 // loads in the lamp objects (made by Ethan), placed at the passed x/y/z coordinates
-function loadLamp(x, y , z) {
+function loadLamp(x, y, z) {
   const loader = new GLTFLoader().setPath( 'gltf/' );
   loader.load( 'lamp2.glb', function ( gltf ) {
 
@@ -164,7 +196,7 @@ function loadLamp(x, y , z) {
 }
 
 // loads in the tree objects, placed at the passed x/y/z coordinates
-function loadTree(x, y , z) {
+function loadTree(x, y, z) {
   const loader = new GLTFLoader().setPath( 'gltf/' );
   loader.load( 'Low_Poly_Tree_GLTF.glb', function ( gltf ) {
 
@@ -174,14 +206,14 @@ function loadTree(x, y , z) {
     model.position.x = x;
     model.position.y = y;
     model.position.z = z;
-    model.rotation.y = Math.random(Math.PI / 3);
+    model.rotation.y = Math.random(Math.PI / 2);
     scene.add( model );
 
   } );
 }
 
 // loads in the dock object (made by Ethan) at the passed x/y/z coordinates
-function loadDock(x, y , z) {
+function loadDock(x, y, z) {
   const loader = new GLTFLoader().setPath( 'gltf/' );
   loader.load( 'dock.glb', function ( gltf ) {
 
@@ -198,7 +230,7 @@ function loadDock(x, y , z) {
 }
 
 // loads in the house object at the passed x/y/z coordinates
-function loadHouse(x, y , z) {
+function loadHouse(x, y, z) {
   const loader = new GLTFLoader().setPath( 'gltf/' );
   loader.load( 'wood_house.glb', function ( gltf ) {
 
@@ -246,18 +278,13 @@ function skyBoxDay() {
 // creates the water and island, and just general scene structure. plane tutorial from
 // https://cobweb.cs.uga.edu/~maria/classes/x810-2023-Fall/09-floor-threeJS.html 
 function makeWorld() {
-  const floor = new THREE.Mesh(new THREE.PlaneGeometry(2000, 2000), 
-  new THREE.MeshLambertMaterial({side: THREE.BackSide, color: "#171452", opacity: 0.9, transparent: true}));
-  floor.position.y = - 120;
-  floor.rotation.x = Math.PI / 2;
-  floor.receiveShadow = true;
-  // surface of water
-  const waterText = new THREE.TextureLoader().load('textures/Water 0339.jpg');
-  const water = new THREE.Mesh(new THREE.PlaneGeometry(2000, 2000), 
-  new THREE.MeshLambertMaterial({side: THREE.DoubleSide, map: waterText, opacity: 0.4, transparent: true}));
+// layer under water surface, gives more depth to the water appearance
+  const underWater = new THREE.Mesh(new THREE.PlaneGeometry(2000, 2000), 
+  new THREE.MeshLambertMaterial({side: THREE.BackSide, color: "#171452", opacity: 0.8, transparent: true}));
+  underWater.position.y = - 120;
+  underWater.rotation.x = Math.PI / 2;
+  underWater.receiveShadow = true;
 
-  water.position.y = - 100;
-  water.rotation.x = Math.PI / 2;
 // bottom of the water
   const bFloor = new THREE.Mesh(new THREE.PlaneGeometry(2000, 2000), 
   new THREE.MeshLambertMaterial({side: THREE.BackSide, color: "#2b2510"}));
@@ -309,8 +336,7 @@ function makeWorld() {
 
   scene.add(sand);
   scene.add(island);
-  scene.add(floor);
-  scene.add(water);
+  scene.add(underWater);
   scene.add(bFloor);
 }
 
@@ -341,11 +367,6 @@ function updatePosition() {
   if (keys['KeyA']) camera.position.add(CalculateRotation(new THREE.Vector3(-1,0,0),camera.rotation,moveSpeed));
   if (keys['KeyD']) camera.position.add(CalculateRotation(new THREE.Vector3(1,0,0),camera.rotation,moveSpeed));
 
-  /* Up & Down (this breaks everything for some reason)
-  if (keys['KeyQ']) camera.position.add(CalculateRotation(new THREE.vector3(0,-1,0),camera.rotation,moveSpeed));
-  if (keys['KeyE']) camera.position.add(CalculateRotation(new THREE.vector3(0,1,0),camera.rotation,moveSpeed));
-  */
-
   // taken from https://codesandbox.io/p/sandbox/clever-andras-k3q078?file=%2Fscript.js%3A39%2C1-48%2C3
   // this rotates the camera relatively well. just try to have the mouse in the center of the screen 
   // when entering the tab, or it will be a bit wonky
@@ -355,11 +376,28 @@ function updatePosition() {
   };
 }
 
+// THIS IS THE ACTUAL WATER SURFACE
+// IT IS OUTSIDE THE MAKEWORLD() SO THAT IT ACTUALLY CHANGES THE TIME VALUE IN THE SHADER EVERY FRAME
+// shader implementation (see index.html) is derived (partly) from https://codepen.io/fjiumqtd-the-decoder/pen/MWRBOxY 
+const water = new THREE.Mesh(new THREE.PlaneGeometry(2000, 2000), 
+new THREE.ShaderMaterial({side: THREE.BackSide, uniforms: {
+  u_time: {value: TIME},
+  u_resolution: {value: windowSize}
+}, fragmentShader: document.getElementById('fragmentShader').textContent, transparent: true}));
+
+water.position.y = - 100;
+water.rotation.x = Math.PI / 2;
+
+scene.add(water);
+
 // renders each frame of the scene and camera's view
 function animate() {
+
+  water.material.uniforms.u_time.value += 0.0015;
 	renderer.render( scene, camera );
   updatePosition();
   window.requestAnimationFrame(animate);
+
 }
 
 animate();
